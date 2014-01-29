9f9905cd7bb6ba73ecfd107b50d7f1c6db229b49
Xiangwen Zhu <intotherain@126.com>
2014-01-29 16:05:13 +0800

    test

--- fCA4HelpDocumentTool/CheckUnusedImages.py
+++ fCA4HelpDocumentTool/CheckUnusedImages.py
@@ -1,8 +1,3 @@
-#! /usr/bin/python
-
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
 
 import os
 import re
